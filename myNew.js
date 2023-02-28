3f73fc6d4dfe1d91111830b866e3c0bf3c45edf3
Batch2206G <batch2206g@gmail.com>
2023-02-28 20:02:51 +0500

    Now all features of calculator have been completed

--- myNew.js
+++ myNew.js
@@ -20,3 +20,26 @@ function minus()
 	alert(result);
 
 }
+
+function multiply()
+{
+	var a = prompt("Enter your first number here ");
+	var b = prompt("Enter your second number here ");
+
+	var result = a*b;
+
+	alert(result);
+
+}
+
+
+function div()
+{
+	var a = prompt("Enter your first number here ");
+	var b = prompt("Enter your second number here ");
+
+	var result = a/b;
+
+	alert(result);
+
+}
