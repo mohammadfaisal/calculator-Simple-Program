ce03cdd34079075b99ff1c9939605575592bfbf1
Batch2206G <batch2206g@gmail.com>
2023-02-28 19:55:46 +0500

    Changes in JS with Minus Function in newbranch

--- myNew.js
+++ myNew.js
@@ -8,3 +8,15 @@ function sum()
 	alert(result);
 
 }
+
+
+function minus()
+{
+	var a = prompt("Enter your first number here ");
+	var b = prompt("Enter your second number here ");
+
+	var result = a-b;
+
+	alert(result);
+
+}
